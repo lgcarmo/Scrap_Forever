48b765ef660b8d254234e70eb0498da61c923781
K40S <48776615+lgcarmo@users.noreply.github.com>
2024-01-01 19:00:03 -0300

    Add threat Executor

--- README.md
+++ README.md
@@ -10,18 +10,24 @@ Get all links in all pages in one application
 
 ## Principais opções
 
-* `-t` SCRAP_URL          https://site.com.br
+* `-t` URL          https://site.com.br
 
-* `-o` SCRAP_OUT, --output SCRAP_OUT Salva data
+* `-o` Output File --output SCRAP_OUT Salva data
 
-* `-p` SCRAP_PROXY, --proxy SCRAP_PROXY  (-p http://127.0.0.1:8080)
+* `-p` Proxy --proxy SCRAP_PROXY  (-p http://127.0.0.1:8080)
+
+* `-n` Threads, -n 10
 
 ### USAGE
 
 
 ```bash
 $ python3 scrap_forever.py -t https://site.com.br -o output.txt
+
+$ python3 scrap_forever.py -t https://site.com.br -o output.txt -n 20
 ```
 ```bash
 $ python3 scrap_forever.py -t https://site.com.br -o output.txt -p http://127.0.0.1:8080
+
+$ python3 scrap_forever.py -t https://site.com.br -o output.txt -p http://127.0.0.1:8080 -n 20
 ```
--- scrap_forever.py
+++ scrap_forever.py
@@ -1,50 +1,46 @@
 import requests
 import re
 import argparse
-from sys import argv
+import concurrent.futures
 import urllib3
-
-
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+parser = argparse.ArgumentParser(description='Requests.')
+parser.add_argument('-t', dest='scrap_url', help='https://site.com.br', required=True)
+parser.add_argument('-o', '--output', type=argparse.FileType(mode='w'), dest='scrap_out', help='Salva data.',
+                    required=True)
+parser.add_argument('-p', '--proxy', dest='scrap_proxy', help='-p http://127.0.0.1:8080', required=False)
+parser.add_argument('-a', '--auth', dest='scrap_auth', help='-a Besic aqsdiqjewqd==', required=False)
+parser.add_argument('-c', '--cookie', dest='scrap_cookie', help='-c PHPSESSION=cookie', required=False)
+parser.add_argument('--user-agent', dest='scrap_user_agent', help='--user-agent Mozilla/5.0', required=False)
+parser.add_argument('-n', '--threads', dest='num_threads', type=int, default=10,
+                    help='Number of threads (default: 10)')
 
+args = parser.parse_args()
 
+if not len(args.scrap_url) > 1:
+    print(''' 
+   python3 forever_scrap.py -h/--help
 
-parser=argparse.ArgumentParser (description='Requests.')
-parser.add_argument ('-t', dest='scrap_url', help='https://site.com.br', required=True)
-parser.add_argument ('-o', '--output', type=argparse.FileType(mode='w'), dest='scrap_out', help='Salva data.', required=True)
-parser.add_argument ('-p', '--proxy',  dest='scrap_proxy', help='-p http://127.0.0.1:8080', required=False)
-parser.add_argument ('-a', '--auth',  dest='scrap_auth', help='-a Besic aqsdiqjewqd==', required=False)
-parser.add_argument ('-c', '--cookie',  dest='scrap_cookie', help='-c PHPSESSION=cookie', required=False)
-parser.add_argument ('--user-agent',  dest='scrap_user_agent', help='--user-agent Mozilla/5.0', required=False)
+    -t Target to web scraping (Ex. -t https://site.com)
+    -o output file (Ex. -o output.txt )
+    -p/--proxy set proxy to request (Ex. -p http://127.0.0.1:8080)
+    -c/--cookie set cookie (Ex. -c Authorization: aqsdiqjewqd==)
+    -n/--threads Number of threads (default: 10)
+    
+    Usage:
+    
+    ./forever_scrap.py -t site.com -o output.txt
+   ''')
 
-args=parser.parse_args()
-
-
-
-if not len(argv) > 1:
-       print(''' 
-       python3 forever_scrap.py -h/--help
-
-        -t Target to web scraping (Ex. -t https://site.com)
-        -o output file (Ex. -o output.txt )
-        -p/--proxy set proxy to request (Ex. -p http://127.0.0.1:8080)
-        -c/--cookie set cookie (Ex. -c Authorization: aqsdiqjewqd==)
-
-        
-        Usage:
-        
-        ./forever_scrap.py -t site.com -o output.txt
-       ''')
-
-#global List of Results
+# global List of Results
 global_url = []
 
-#url base request
+# url base request
 first_url = args.scrap_url
 
-#request and return html
+# request and return html
 def resq_urls(url_x):
     headers = {
         'authorization': f'{args.scrap_auth}',
@@ -52,80 +48,60 @@ def resq_urls(url_x):
         'user-agent': f'{args.scrap_user_agent}'
     }
     if args.scrap_proxy is None:
-
         response = requests.get(url_x, verify=False, headers=headers)
-        return(response.text)
-
+        return response.text
     elif args.scrap_proxy is not None:
-        proxy = {'http': f'{args.scrap_proxy}','https': f'{args.scrap_proxy}'}
+        proxy = {'http': f'{args.scrap_proxy}', 'https': f'{args.scrap_proxy}'}
         response = requests.get(url_x, proxies=proxy, verify=False, headers=headers)
-        return (response.text)
+        return response.text
 
-
-#Def find URL in pag
+# Def find URL in pag
 def find_url(response_x):
-    comp = re.compile("https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)")
+    comp = re.compile(
+        "https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0.9()]{1,6}\\b(?:[-a-zA-Z0.9()@:%_\\+.~#?&\\/=]*)")
     x = re.findall(comp, response_x)
-
     for list in x:
         if first_url in list:
             if list not in global_url:
                 global_url.append(list)
-            else:
-                pass
-        else:
-            pass
 
-#Def get HREF
+# Def get HREF
 def find_href(response_x):
     comp_href = re.compile(r'href=[\'"]?([^\'" >]+)')
     x = re.findall(comp_href, response_x)
-
     for list in x:
-            if f'{first_url}/{list}' not in global_url:
-                if 'http' in f'{list}':
-                    if first_url not in list:
-                        pass
-                    else:
-                        global_url.append(f'{list}')
+        if f'{first_url}/{list}' not in global_url:
+            if 'http' in f'{list}':
+                if first_url not in list:
+                    pass
+                else:
+                    global_url.append(f'{list}')
+            elif 'https' in f'{list}':
+                if first_url not in list:
+                    pass
+                else:
+                    global_url.append(f'{list}')
+            elif '/' in f'{first_url}/{list}':
+                global_url.append(f'{first_url}/{list}')
 
-                elif 'https' in f'{list}':
-                    if first_url not in list:
-                        pass
-                    else:
-                        global_url.append(f'{list}')
-
-                elif '/' in f'{first_url}/{list}':
-                    global_url.append(f'{first_url}/{list}')
-            else:
-                pass
-
-
-
-#base first Request
+# base first Request
 resq_urls(first_url)
 find_url(resq_urls(first_url))
 find_href(resq_urls(first_url))
 
+# Collect All Url in All Pages using multi-threading
+def process_url(url):
+    find_url(resq_urls(url))
+    find_href(resq_urls(url))
+    print(f'List URL: {url}', end='\r', flush=True)
+    args.scrap_out.write(url + '\n')
 
-#Collect All Url in All Pages
-n=0
-try:
-    while n < len(global_url):
+with concurrent.futures.ThreadPoolExecutor(max_workers=args.num_threads) as executor:
+    urls_to_process = global_url.copy()
+    future_to_url = {executor.submit(process_url, url): url for url in urls_to_process}
+    concurrent.futures.wait(future_to_url)
 
-            find_url(resq_urls(global_url[n]))
-            find_href(resq_urls(global_url[n]))
-            print(f'List URL: {global_url[n]}', end='\r', flush=True)
-            args.scrap_out.write(global_url[n]+'\n')
-            n+=1
-
-except (KeyboardInterrupt):
-    print('[-] STOP')
-
-
-# tratar href que contain http e https.
-#pensar em criar duas lista um com apenas o dominio e outra apenas com os possiveis falso positovos.
-
-
+# tratar href that contain http and https.
+# think about creating two lists, one with only the domain and another with possible false positives.
 for out in global_url:
     print(out)
